ff9ee144d668ccb11f348a8024922696f7084553
Georgios <mastrog86@yahoo.com>
2021-08-08 15:29:04 +0200

    modified it to become stop watch

--- script.js
+++ script.js
@@ -1,36 +1,68 @@
-class Clock extends React.Component {
-    constructor (props) {
+class StopWatch extends React.Component {
+    constructor(props) {
       super(props);
-      this.state = { date: new Date() }; // this is the component state property object
+      this.state = {
+        timePassedInMilliSeconds: 0
+      }
+  
+      this.timer = null;
+  
+      this.start = this.start.bind(this);
+      this.stop = this.stop.bind(this);
+      this.reset = this.reset.bind(this);
     }
   
-    componentDidMount () {
-      this.timer = setInterval(
-        () => this.updateTime(),
-        1000
-      );
+    start() {
+      if (!this.timer) {
+        let startTime = Date.now();
+        this.timer = setInterval(() => {
+          const stopTime = Date.now();
+          const timePassedInMilliSeconds = stopTime - startTime + this.state.timePassedInMilliSeconds;
+  
+          this.setState({
+            timePassedInMilliSeconds,
+          });
+          
+          startTime = stopTime;
+        }, 250); // Executed every 250 millisecond
+      }
     }
   
-    componentWillUnmount () {
-      clearInterval(this.timer);
+    stop() {
+      window.clearInterval(this.timer);
+      this.timer = null;
     }
   
-    updateTime () {
+    reset() {
+      this.stop();
       this.setState({
-        date: new Date()
-      });
+        timePassedInMilliSeconds: 0
+      })
     }
   
-    render () {
+    render() {
       return (
         <div>
-          <h2>The time now is {this.state.date.toLocaleTimeString()}.</h2>
+          <h2 className="border px-3 py-4 rounded my-3 mx-auto text-center" style={{maxWidth: "300px"}}>
+            {Math.floor(this.state.timePassedInMilliSeconds / 1000)} s
+          </h2>
+          <div className="d-flex justify-content-center">
+            <button className="btn btn-outline-primary mr-2" onClick={this.start}>
+              start
+            </button>
+            <button className="btn btn-outline-danger mr-2" onClick={this.stop}>
+              stop
+            </button>
+            <button className="btn btn-outline-warning" onClick={this.reset}>
+              reset
+            </button>
+          </div>
         </div>
       )
     }
   }
-  
+
   ReactDOM.render(
-    <Clock />,
+    <StopWatch />,
     document.getElementById('root')
   );
